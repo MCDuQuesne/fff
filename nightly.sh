eb9cc3611f321122b2b886698f6b179385c55d66
MCDuQuesne <86164016+MCDuQuesne@users.noreply.github.com>
2021-06-20 00:05:22 -0700

    Create nightly.sh
    creating

---
+++ nightly.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo -n Nightly Maintenance started: 
+date
+backup="/var/www/peertube/storage/backup"
+ptdir="/var/www/peertube"
+echo ==== Copying current files to backup directory ====
+cp $ptdir/config/*.yaml $backup/config/
+cp $ptdir/config/*.json $backup/config/
+cp /etc/nginx/sites-available/peertube $backup/
+cp /etc/fstab $backup/
+cp $ptdir/*.sh $backup/scripts/
+cp /etc/systemd/system/rclone.service $backup/services/
+cp /etc/systemd/system/peertube.service $backup/services/
+crontab -l > $backup/crontab.backup
+echo ==== Backing up database  ====
+pg_dump -Fc peertube_prod > $backup/peertube_prod-dump.db
+cd $ptdir/storage
+echo ==== synching offsite storage  ====
+rsync -Pur * $ptdir/offsitestorage/
+echo ==== deleting local videos older than a week  ====
+find $ptdir/storage/videos/* -mtime +7 -exec rm {} \;
+find $ptdir/storage/streaming-playlists/hls/* -mtime +7 -exec rm -r {} \;
+echo -n Nightly Maintenance ended: 
+date
