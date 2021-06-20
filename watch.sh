a5860a083dae855ba0c8116b8616c1d9e0da77b7
MCDuQuesne <86164016+MCDuQuesne@users.noreply.github.com>
2021-06-20 00:32:58 -0700

    Create watch.sh
    added watch

---
+++ watch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo -n Starting to watch : 
+date
+watch="/var/www/peertube/storage/videos"
+while true
+do
+    fileName=$(inotifywait -r -e create $watch | sed -r 's/^.*CREATE(,ISDIR)*\s+(.*)$/\2/g')
+    now=$(date)
+    echo -n " $now "
+    if grep -Fxq "$fileName" /var/www/peertube/storage/logs/transferedUUID.log
+    then
+        echo "already offloaded $fileName"
+    else
+        rsync -Pur -e "ssh -i /var/www/peertube/trans" /var/www/peertube/storage/videos/$fileName red@172.111.140.236:/home/red/totranscode/
+        echo $fileName >> /var/www/peertube/storage/logs/transferedUUID.log
+    fi
+done
