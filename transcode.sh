66d6d6bd82c700c636133b45d4fc782dae3de886
MCDuQuesne <86164016+MCDuQuesne@users.noreply.github.com>
2021-06-21 02:48:32 -0700

    Update transcode.sh

--- transcode.sh
+++ transcode.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-while true; do
+while true
+do
     now=$(date)
     echo -n $now
     cd /home/red/totranscode
-    for fileName in *.mp4; do
-        if [[ "$fileName" == "*.mp4" ]];
-            then echo ": nothing found"
+    for fileName in *.mp4
+    do
+        if [[ "$fileName" == "*.mp4" ]]
+        then 
+            echo ": nothing found"
             sleep 600
             continue 2
         fi
-#       parts=$(echo $fileName | tr "." "\n")
         uuid=${fileName:0:36}
-#       res=${fileName:37:4}
-        echo -n " $uuid"
-#       echo res: $res
-#       echo filename: $fileName
+        res=${fileName:37:4}
         /usr/bin/ffmpeg -i $fileName \
                 -y -acodec copy -vcodec libx264 -threads 8 -f mp4 -movflags faststart \
                 -max_muxing_queue_size 1024 -map_metadata -1 -b_strategy 1 -bf 16 -pix_fmt yuv420p \
@@ -25,7 +24,7 @@ while true; do
         /usr/bin/ffmpeg -i $fileName \
                 -y -acodec copy -vcodec libx264 -threads 8 -f mp4 -movflags faststart \
                 -max_muxing_queue_size 1024 -map_metadata -1 -b_strategy 1 -bf 16 -pix_fmt yuv420p \
-                -vf scale=w=-1:h=144 -preset veryfast \
+                -vf scale=w=-1:h=144 -preset veryslow -ab 96 -crf 42 \
                 -level:v 3.1 -g:v 60 -hls_time 4 -hls_list_size 0 -hls_playlist_type vod \
                 -hls_segment_filename /home/red/done/$uuid-small.mp4 \
                 -hls_segment_type fmp4 -f hls -hls_flags single_file temp.m3u8
