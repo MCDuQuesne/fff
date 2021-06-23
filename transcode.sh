607f842be70357e15f9377255b69fe370074c868
MCDuQuesne <86164016+MCDuQuesne@users.noreply.github.com>
2021-06-22 18:33:41 -0700

    tweaking ffmpeg

--- transcode.sh
+++ transcode.sh
@@ -21,12 +21,20 @@ do
                 -level:v 3.1 -g:v 60 -hls_time 4 -hls_list_size 0 -hls_playlist_type vod \
                 -hls_segment_filename /home/red/done/$uuid.mp4 \
                 -hls_segment_type fmp4 -f hls -hls_flags single_file temp.m3u8
+        #experimenting with lowest bandwidth options"
         /usr/bin/ffmpeg -i $fileName \
                 -y -acodec copy -vcodec libx264 -threads 8 -f mp4 -movflags faststart \
                 -max_muxing_queue_size 1024 -map_metadata -1 -b_strategy 1 -bf 16 -pix_fmt yuv420p \
-                -vf scale=w=-1:h=144 -preset veryslow -crf 42 \
+                -vf scale=w=-1:h=144 -preset veryfast -crf 49 \
                 -level:v 3.1 -g:v 60 -hls_time 4 -hls_list_size 0 -hls_playlist_type vod \
                 -hls_segment_filename /home/red/done/$uuid-small.mp4 \
+                -hls_segment_type fmp4 -f hls -hls_flags single_file temp.m3u8
+        /usr/bin/ffmpeg -i $fileName \
+                -y -acodec copy -vcodec libx264 -threads 8 -f mp4 -movflags faststart \
+                -max_muxing_queue_size 1024 -map_metadata -1 -b_strategy 1 -bf 16 -pix_fmt yuv420p \
+                -vf scale=w=-1:h=64 -preset veryslow -crf 18 \
+                -level:v 3.1 -g:v 60 -hls_time 4 -hls_list_size 0 -hls_playlist_type vod \
+                -hls_segment_filename /home/red/done/$uuid-tiny.mp4 \
                 -hls_segment_type fmp4 -f hls -hls_flags single_file temp.m3u8
         mv $fileName /home/red/todelete/
     done
